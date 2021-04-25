5478e0aeee61c693d770709a49a0147b94591e02
Alina Butba <alina.butba@yandex.ru>
2021-04-25 05:24:59 +0300

    Внести изменения после первой проверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,32 @@
 const openPopupEdit = document.querySelector('.person-card__edit-button');
 const overlay = document.querySelector('.overlay');
 const popUpEditButton = document.querySelector('.popup-edit__close-button');
-const formSubmitButton = document.querySelector('.form-edit__save-button');
-const formName = document.querySelector('.form-edit__name');
-const formJob = document.querySelector('.form-edit__job'); 
+const formSubmit = document.querySelector('.form-edit');
+const formName = document.querySelector('#form1-name');
+const formJob = document.querySelector('#form1-job'); 
 const cardName = document.querySelector('.person-card__name');
 const cardJob = document.querySelector('.person-card__job');
-const sendButton = document.querySelector('.form-edit__save-button');
+const formEdit = document.querySelector('.form-edit')
 
 
 function formSubmitHandler (evn) {
     evn.preventDefault();
     cardName.textContent = formName.value;
     cardJob.textContent = formJob.value;
-    overlay.classList.toggle('overlay_is-opened');
+    overlay.classList.toggle('overlay_opened');
+    console.log('overlay_opened');
 }
 
 
 
 function togglePopUp(evn) {
     evn.preventDefault();
-    overlay.classList.toggle('overlay_is-opened');
+    overlay.classList.toggle('overlay_opened');
     formName.value = cardName.textContent;
-    formJob.value = cardJob.textContent;          
+    formJob.value = cardJob.textContent;
+    console.log('overlay_opened');
 }
 
 openPopupEdit.addEventListener('click', togglePopUp);
-popUpEditButton.addEventListener("click", togglePopUp);   
-sendButton.addEventListener('click', formSubmitHandler);
+popUpEditButton.addEventListener('click', togglePopUp);   
+formSubmit.addEventListener('submit', formSubmitHandler);
